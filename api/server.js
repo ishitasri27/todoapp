eee3d15be95154eaba6cdb30813ecadb69af0fcb
Ishita Srivastava <ishitasri7451@gmail.com>
2022-11-30 10:35:22 +0530

    next commit

--- api/server.js
+++ api/server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+require("dotenv").config();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+const port = process.env.PORT || 5000; 
+
 mongoose
-  .connect("mongodb://127.0.0.1:27017/mern-todo", {
+  .connect(process.env.MONGODB_URL, {
     useNewurlParser: true,
     useUnifiedTopology: true,
   })
@@ -45,4 +47,4 @@ app.get("/todo/complete/:id/:type", async (req, res) => {
   res.json(todo);
 });
 
-app.listen(3001, () => console.log("Server started on port 3001"));
+app.listen(port, () => console.log(`Server started on port ${port}`));
